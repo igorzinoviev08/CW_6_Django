4ea851754e369f5f270870b620f24b636a10b49f
igorzinoviev08 <zinovievigor08@gmail.com>
2023-11-25 01:42:03 +0300

    добавлена логика рассылки

--- mailing/apps.py
+++ mailing/apps.py
@@ -1,6 +1,13 @@
+from time import sleep
+
 from django.apps import AppConfig
 
 
 class MailingConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'mailing'
+
+    def ready(self):
+        from jobs import updater
+        sleep(2)
+        updater.start()
